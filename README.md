254daebc1bb822851d0165383bd90a6d42def38e
mehlj <mehlsec@protonmail.com>
2021-02-08 16:44:34 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # duplicate_ckl
-Duplicate CKL files according to a CSV list of machines
+Duplicate CKL files according to a CSV list of machines.
+
+For situations wherein one checklist is completed and is representative of many other systems, but individual .ckl files
+must be created anyways. 
+
+## Usage
+
+1. Complete one manual checklist.
+2. Create the `list` CSV file and add every machine that is a duplicate of that completed system. Ex:
+    ```
+    machine1,192.168.1.6,38:D5:47:7A:46:B6
+    machine2,192.168.1.7,38:D5:47:7A:46:B5
+    machine3,192.168.1.8,38:D5:47:7A:46:B4
+    machine4,192.168.1.9,38:D5:47:7A:46:B3
+    testbox,192.168.1.10,38:D5:47:7A:46:B2
+    mehljbox,192.168.1.11,38:D5:47:7A:46:B1
+    ```
+3. Run the script, specifying both of these files:
+    ```
+    $ python main.py machines windows10.ckl
+    ```
+4. The new checklists will be located in your current directory, under `./checklists/`. 
+Each checklist will contain the same data as the original completed one, except with unique hostname/MAC/IP info.
+    ```
+    $ ls ./checklists/
+    machine1.ckl machine2.ckl machine3.ckl machine4.ckl testbox.ckl mehljbox.ckl 
+    ```
---
+++ main.py
@@ -0,0 +1,48 @@
+import csv
+import argparse
+import shutil
+import os
+import re
+
+parser = argparse.ArgumentParser(description='Duplicate CKL files according to a CSV list of machines.')
+
+parser.add_argument('list',
+                    metavar='list',
+                    type=str,
+                    help='name of the CSV file containing machine information')
+
+parser.add_argument('ckl',
+                    metavar='ckl',
+                    type=str,
+                    help='name of the CKL file to duplicate')
+
+args = parser.parse_args()
+input_list = args.list
+input_ckl = args.ckl
+
+file = csv.reader(open(input_list), delimiter=',')
+
+# create output directory
+try:
+    os.makedirs('checklists/', exist_ok=True)
+except OSError as error:
+    print(error)
+
+# start looping and duplicating CKLs
+for line in file:
+    # copy CKL from user-provided master copy
+    new_ckl = "checklists/" + line[0] + ".ckl"
+    shutil.copy2(input_ckl, new_ckl)
+
+    # read the newly copied CKL into memory
+    with open(input_ckl, 'r') as file:
+        ckl_text = file.read()
+
+    # modify data in place
+    ckl_text = re.sub(r'<HOST_NAME>(.*)</HOST_NAME>', '<HOST_NAME>' + line[0] + '</HOST_NAME>', ckl_text)
+    ckl_text = re.sub(r'<HOST_IP>(.*)</HOST_IP>', '<HOST_IP>' + line[1] + '</HOST_IP>', ckl_text)
+    ckl_text = re.sub(r'<HOST_MAC>(.*)</HOST_MAC>', '<HOST_MAC>' + line[2] + '</HOST_MAC>', ckl_text)
+
+    ckl_out = open(new_ckl, 'w')
+    ckl_out.writelines(ckl_text)
+    ckl_out.close()
